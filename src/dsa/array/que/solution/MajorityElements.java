1a15ca59898e84dd3f0a93c5fdb2bc87c1d8477b
Aryan Mangal <aryan.mangal9413@gmail.com>
2023-08-09 14:01:03 +0530

    Majority Element Solution Move's Voting algorithm

--- src/dsa/array/que/solution/MajorityElements.java
+++ src/dsa/array/que/solution/MajorityElements.java
@@ -22,19 +22,45 @@ public class MajorityElements {
 //        return temp;
 		
 //		Better Approach using Hashing
-		HashMap<Integer, Integer> mpp = new HashMap<>();
+//		HashMap<Integer, Integer> mpp = new HashMap<>();
+//		
+////		Storing elements with its occurences:
+//		for(int i=0; i<n; i++) {
+//			int value = mpp.getOrDefault(nums[i], 0);
+//			mpp.put(nums[i], value+1);
+//		}
+////		Searching for Majority Element
+//		for(Map.Entry<Integer, Integer> it: mpp.entrySet()) {
+//			if(it.getValue()> n/2) {
+//				return it.getKey();
+//			}
+//		}
+//		return -1;
 		
-//		Storing elements with its occurences:
-		for(int i=0; i<n; i++) {
-			int value = mpp.getOrDefault(nums[i], 0);
-			mpp.put(nums[i], value+1);
-		}
-//		Searching for Majority Element
-		for(Map.Entry<Integer, Integer> it: mpp.entrySet()) {
-			if(it.getValue()> n/2) {
-				return it.getKey();
-			}
-		}
-		return -1;
+//		Optimal Approach- Move's Voting algorithm
+		// Moovs voting algorithm
+        int candidate = 0;
+        int count =0;
+        for(int i =0; i<n; i++){
+            if(count ==0){
+                count =1;
+                candidate = nums[i];
+                count++;
+            }else if(candidate == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        //checking if the stored element
+        // is the majority element:
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == candidate) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return candidate;
+        return -1;
 	}
 }
